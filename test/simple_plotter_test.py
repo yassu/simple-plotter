5123fdd8b50a2324a3891e3e912d729ce739bc1c
yassu <mathyassu@gmail.com>
2015-03-27 10:39:34 +0900

    fix test name

--- test/simple_plotter_test.py
+++ test/simple_plotter_test.py
@@ -3,11 +3,11 @@ path.append('src')
 from simple_plotter import get_varnames
 
 
-def get_names_test1():
+def get_varnames_test1():
     assert(get_varnames('x**2 + y**3') == {'x', 'y'})
 
-def get_names_test2():
+def get_varnames_test2():
     assert(get_varnames('t**2') == {'t'})
 
-def get_names_test3():
+def get_varnames_test3():
     assert(get_varnames('sin(x**2)') == {'x'})
